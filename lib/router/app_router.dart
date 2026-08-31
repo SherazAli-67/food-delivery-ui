77480236929fbd5389e0d85a75fd89d2b8343734
sheraz-ali <soomrosheraz054@gmail.com>
2026-08-31 23:38:20 +0500

    Food Delivery App - Development
    1. Navigation menu development

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,15 +1,41 @@
+import 'package:flutter/cupertino.dart';
 import 'package:food_delivery_ui/presentation/screens/welcome_screen.dart';
 import 'package:go_router/go_router.dart';
 
 GoRouter router = GoRouter(
     initialLocation: NamedRoutes.welcome.routeName,
     routes: [
-      GoRoute(path: NamedRoutes.welcome.routeName, builder: (_, _)=> WelcomeScreen())
+      GoRoute(path: NamedRoutes.welcome.routeName, builder: (_, _)=> WelcomeScreen()),
+      StatefulShellRoute.indexedStack(
+          branches: [
+        StatefulShellBranch(routes: [
+          GoRoute(path: NamedRoutes.home.routeName, builder: (_, _)=> Center(child: Text("Home"),))
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(path: NamedRoutes.delivery.routeName, builder: (_, _)=> Center(child: Text("Delivery"),))
+
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(path: NamedRoutes.bookmark.routeName, builder: (_, _)=> Center(child: Text("Bookmark"),))
+
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(path: NamedRoutes.profile.routeName, builder: (_, _)=> Center(child: Text("Profile"),))
+
+        ]),
+      ],
+        builder: (ctx, state, navigationShell) => Center(child: Text("Main Menu page"),)
+      )
     ],
 );
 
 enum NamedRoutes {
-  welcome('/welcome');
+  welcome('/welcome'),
+  home('/home'),
+  delivery('/delivery'),
+  bookmark('/bookmark'),
+  profile('/profile')
+  ;
   
   final String routeName;
   const NamedRoutes(this.routeName);
